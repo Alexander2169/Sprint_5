70e84c5ec30b48a2073a00123a7d37afef2ba0d9
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 20:47:56 +0300

    add files config.py and new conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium import webdriver
-
+from config import BASE_URL
 @pytest.fixture()
 def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--window-size=1920,1080')
     chrome_options.add_argument("--incognito")
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.get(BASE_URL)
     yield driver
     driver.quit()
 
