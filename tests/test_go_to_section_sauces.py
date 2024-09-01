f474a9756edda81969ee70ad2b26a9482782ab62
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 16:48:14 +0300

    add test sauces

---
+++ tests/test_go_to_section_sauces.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from locators import Locators
+from conftest import driver
+import time
+
+class TestGoToSection:
+    def test_go_to_the_buns_section(self, driver):
+        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(Locators.button_login_main_page))
+        driver.find_element(*Locators.fillings_section).click()
+        driver.find_element(*Locators.sauces_section).click()
+        assert driver.find_element(*Locators.selected_button).text == "Соусы"
+        time.sleep(3)
