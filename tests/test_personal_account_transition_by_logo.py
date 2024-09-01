520df8e44d73b32f3a4d97d37339f8726421ccf9
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 15:45:57 +0300

    test update 8

---
+++ tests/test_personal_account_transition_by_logo.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from locators import Locators
+from conftest import driver
+import time
+
+class TestNavigateToConstructor:
+    def test_personal_account_transition_by_clicking_on_the_constructor(self, driver):
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.button_personal_account))
+        driver.find_element(*Locators.button_personal_account).click()
+        driver.find_element(*Locators.input_email_login).send_keys('alexander_cheremisov_13_777@mail.ru')
+        driver.find_element(*Locators.input_password_login).send_keys('281077')
+        driver.find_element(*Locators.button_login).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.button_login))
+        driver.find_element(*Locators.logo).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.logo))
+        assert driver.find_element(*Locators.logo).is_displayed()
+        time.sleep(3)
