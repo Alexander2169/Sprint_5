cfe7a4eb43490625d6631afd13c1351ce3ae9c03
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 16:06:23 +0300

    test update 9

---
+++ tests/test_logout.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from locators import Locators
+from conftest import driver
+import time
+
+class TestGoToPersonalAccount:
+    def test_login_to_your_personal_account(self, driver):
+        WebDriverWait(driver, 10).until(
+        expected_conditions.visibility_of_element_located(Locators.button_personal_account))
+        driver.find_element(*Locators.button_personal_account).click()
+        driver.find_element(*Locators.input_email_login).send_keys('alexander_cheremisov_13_777@mail.ru')
+        driver.find_element(*Locators.input_password_login).send_keys('281077')
+        driver.find_element(*Locators.button_login).click()
+        driver.find_element(*Locators.button_personal_account).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.profile))
+        driver.find_element(*Locators.button_logout).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.button_login))
+        assert driver.find_element(*Locators.button_login).is_displayed()
+        time.sleep(3)
+
+
