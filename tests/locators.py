c6a5b073b5434e1c7e103011aaca49908cc78ba2
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 20:38:50 +0300

    add new locators logo

--- tests/locators.py
+++ tests/locators.py
@@ -61,7 +61,7 @@ class Locators:
     fillings_section = (By.XPATH, '//span[text()="Начинки"]')
 
     # Логотип на главной странице сервиса
-    logo = (By.XPATH, '//div[@class="AppHeader_header__logo__2D0X2"]')
+    logo = (By.XPATH, '//div[contains(@class, "AppHeader_header__logo")]')
 
     # Активный раздел конструктора
     selected_button = (By.XPATH, '//div[@class="tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect"]')
