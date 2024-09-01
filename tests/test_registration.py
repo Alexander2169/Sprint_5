36cccde1e392da01c345ab66ade216280cd22a50
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 08:16:59 +0300

    delete file data

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -6,6 +6,7 @@ from conftest import driver
 
 
 
+
 class TestRegistration:
     # Регистрация аккаунта пользователя с валидными значениями инпутов
     def test_registration_new_account_success_submit(self, driver):
@@ -13,11 +14,11 @@ class TestRegistration:
         WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(TestLocators.button_register))
         driver.find_element(*TestLocators.button_register).click()
         WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(TestLocators.button_submit))
-        driver.find_element(*TestLocators.input_name).send_keys(UsersTestData.username)
-        driver.find_element(*TestLocators.input_email).send_keys(random_email)
-        driver.find_element(*TestLocators.input_password).send_keys(random_password)
+        driver.find_element(*TestLocators.input_name).send_keys('alexander_cheremisov')
+        driver.find_element(*TestLocators.input_email).send_keys('alexander_cheremisov_13_77777@mail.ru')
+        driver.find_element(*TestLocators.input_password).send_keys('281077')
         driver.find_element(*TestLocators.button_submit).click()
-        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(TestLocators.button_login))
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(TestLocators.button_login))
         assert driver.find_element(*TestLocators.button_register).is_displayed()
 
 
