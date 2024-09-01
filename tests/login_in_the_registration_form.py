c79ee4400c9214597bb0f66b2f9f4c1a040581a4
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 13:06:34 +0300

    test update 4

--- tests/login_in_the_registration_form.py
+++ tests/login_in_the_registration_form.py
@@ -4,9 +4,12 @@ from locators import Locators
 from conftest import driver
 import time
 class TestEntrance:
-    def test_login_to_your_personal_account(self, driver):
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.button_personal_account))
-        driver.find_element(*Locators.button_personal_account).click()
+    def test_login_in_the_registration_form(self, driver):
+        driver.find_element(*Locators.button_login_main_page).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.button_register_1))
+        driver.find_element(*Locators.button_register_1).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.button_login_in_registration))
+        driver.find_element(*Locators.button_login_in_registration).click()
         driver.find_element(*Locators.input_email_login).send_keys('alexander_cheremisov_13_777@mail.ru')
         driver.find_element(*Locators.input_password_login).send_keys('281077')
         driver.find_element(*Locators.button_login).click()
