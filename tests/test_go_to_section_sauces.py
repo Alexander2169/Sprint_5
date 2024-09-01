6e4f847507ae26e0d6a43c88465dac9e27b94fe8
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-01 16:49:17 +0300

    a new test sauces

--- tests/test_go_to_section_sauces.py
+++ tests/test_go_to_section_sauces.py
@@ -5,7 +5,7 @@ from conftest import driver
 import time
 
 class TestGoToSection:
-    def test_go_to_the_buns_section(self, driver):
+    def test_go_to_the_sauces_section(self, driver):
         WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(Locators.button_login_main_page))
         driver.find_element(*Locators.fillings_section).click()
         driver.find_element(*Locators.sauces_section).click()
