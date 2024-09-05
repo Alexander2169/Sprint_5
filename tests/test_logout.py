1f8691fb6da2860b4740caae780f8c19f2afab9c
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-05 03:24:01 +0300

    improving the test logout.py

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -7,14 +7,11 @@ from helpers import get_sign_up_data
 class TestLogout:
     def test_logout(self, driver):
         name_data, email_data, password_data = get_sign_up_data()
-        driver.find_element(*Locators.button_personal_account).click()
+        WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable(Locators.button_personal_account)).click()
         driver.find_element(*Locators.input_email_login).send_keys(email_data)
         driver.find_element(*Locators.input_password_login).send_keys(password_data)
         driver.find_element(*Locators.button_login).click()
-        driver.find_element(*Locators.button_personal_account).click()
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.profile))
-        driver.find_element(*Locators.button_logout).click()
-        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.button_login))
+        WebDriverWait(driver, 6).until(expected_conditions.visibility_of_element_located(Locators.button_login))
         assert driver.find_element(*Locators.button_login).is_displayed()
 
 
